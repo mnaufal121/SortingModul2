1297759732021015977332a985d9232f11e268cd
Naufal <>
2019-11-24 21:50:07 +0700

    coba 1

---
+++ src/Program2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Asus
+ */
+public class Program2 {
+
+    public static void main(String[] args) {
+        int n = 250;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j+=2) {
+                    System.out.println("i = " + i + ", j = " + j);
+            }
+        }
+        System.out.println("");
+    }
+}
